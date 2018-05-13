1d5cfc3963705037244c591263b64e619e071b81
tracek <lukasz.tracewski@outlook.com>
2018-05-13 08:44:12 +0200

    join R results with labels

---
+++ birdutils.py
@@ -0,0 +1,20 @@
+import datetime
+import pandas as pd
+
+
+def _time_to_total_seconds_converter(val):
+    t = datetime.datetime.strptime(val, '%H.%M.%S.%f').time()
+    total_seconds = t.hour * 3600 + t.minute * 60 + t.second + t.microsecond / 10**6
+    return total_seconds
+
+
+def read_labels(path: str, sheetname: str = None) -> pd.DataFrame:
+    """
+    Read labels from Excel file.
+    :param path: path
+    :param sheetname: optional name of the sheet to load
+    :return: pandas dataframe
+    """
+    sheets = pd.read_excel(path, sheet_name=sheetname, converters={'Time Start': _time_to_total_seconds_converter,
+                                                             'Time End': _time_to_total_seconds_converter})
+    return sheets
---
+++ join.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from birdutils import read_labels
+
+features = pd.read_csv('/mnt/data/Birdman/results/warbler/features.csv')
+labels_dict = read_labels('/mnt/data/Birdman/sthelena_labels.xls')
+feature_group = features.groupby('sound.files', group_keys=True)
+labels_all = pd.concat(labels_dict, axis=0, ignore_index=True)
+print('Found signals: ', len(features))
+print('Labelled calls: ', len(labels_all))
+
+def find_overlap(group):
+    name = os.path.splitext(group.name)[0]
+    labels = labels_dict[name]
+    label_start = labels['Time Start']
+    label_end = labels['Time End']
+
+    result = []
+
+    for index, row in group.iterrows():
+        start = row['start']
+        end = row['end']
+        overlap = any((label_start <= end) & (label_end >= start))
+        result.append(overlap)
+        # print('start {} end {} overlap {}'.format(start, end, overlap))
+    return result
+
+x=feature_group.apply(find_overlap)
