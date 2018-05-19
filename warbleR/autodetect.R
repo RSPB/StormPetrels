431b9ccb4ed01455cf14f856a04fc4382989e613
tracek <lukasz.tracewski@outlook.com>
2018-05-19 12:28:51 +0200

    manual analysis

--- warbleR/autodetect.R
+++ warbleR/autodetect.R
@@ -2,22 +2,23 @@ library(warbleR)
 library(data.table)
 library(stats)
 
-setwd(file.path("/mnt/data/Birdman/test"))
+setwd(file.path("/mnt/data/Birdman/16khz"))
 
 df.audio <- autodetec(threshold = 4, envt = "abs", ssmooth = 1200, power=1,
                       bp=c(1,8), xl = 2, picsize = 2, res = 200, 
                       flim= c(1,8), osci = FALSE, redo = TRUE,
                       wl = 256, ls = TRUE, sxrow = 10, rows = 8, 
-                      mindur = 0.1, maxdur = 1, set = TRUE, parallel = 1)
+                      mindur = 0.1, maxdur = 1, set = TRUE, parallel = 10)
 
 df.audio <- df.audio[complete.cases(df.audio),]
-df.features <- specan(X = df.audio, bp=c(1,8), fast = FALSE, parallel = 1, wl = 256, threshold = 10)
+df.audio$sound.files <- tools::file_path_sans_ext(df.audio$sound.files)
+df.features <- specan(X = df.audio, bp=c(1,8), fast = FALSE, parallel = 10, wl = 256, threshold = 4)
 dt.features <- data.table(df.features) 
 
 dt.features$start <- df.audio$start
 dt.features$end <- df.audio$end
 
 write.table(dt.features,
-            file = "features_all.csv",
+            file = "features.csv",
             row.names=FALSE, na="",col.names=TRUE, sep=",")
 
